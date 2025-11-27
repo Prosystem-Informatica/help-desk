0f75cef8fa954fdf0e62b730171b9296fd823ae7
Lucas Henrique <lucas.dpl@gmail.com>
2025-11-27 14:59:28 -0300

    fix:version vercel .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { EmployeesModule } from './employees/employees.module';
 import { ClientModule } from './client/client.module';
 import { AttachmentsModule } from './attachments/attachments.module';
@@ -9,22 +11,35 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', 
-      host: 'localhost',
-      port: 5432, 
-      username: 'postgres',
-      password: 'admin',
-      database: 'postgres',
-      autoLoadEntities: true, 
-      synchronize: true, 
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<'postgres'>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        ssl: configService.get<boolean>('DB_SSL')
+          ? { rejectUnauthorized: false }
+          : false,
+        autoLoadEntities: true,
+        synchronize: configService.get<boolean>('DB_SYNC'),
+        logging: configService.get<boolean>('DB_LOG'),
+      }),
+      inject: [ConfigService],
+    }),
+
     EmployeesModule,
     ClientModule,
     AttachmentsModule,
     SectorsModule,
     TicketsModule,
-    AuthModule
+    AuthModule,
   ],
 })
 export class AppModule {}
