30a49c03acbccc7fb175b1db1b7a502b57c86543
Giovanni Zaghi <vanni.freitas@hotmail.com>
2025-12-05 12:49:51 -0300

    fix: add api

---
+++ api/index.ts
@@ -0,0 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+
+let cachedApp: express.Express;
+
+async function bootstrap(): Promise<express.Express> {
+  if (!cachedApp) {
+    const expressApp = express();
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+
+    await app.init();
+
+    cachedApp = expressApp;
+  }
+  return cachedApp;
+}
+
+export default async (req: express.Request, res: express.Response) => {
+  const app = await bootstrap();
+  return app(req, res);
+};
