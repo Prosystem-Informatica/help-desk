79773daa92c824d5956e48bf80b4ef13bb7b8db9
Giovanni Zaghi <vanni.freitas@hotmail.com>
2025-12-06 15:42:20 -0300

    fix: service att

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, Inject, forwardRef } from '@nestjs/common';
+import { ConflictException, Injectable, Inject, forwardRef, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Employees } from './employees.entity';
@@ -6,6 +6,7 @@ import { CreateEmployeesDto } from './dto/create-employees.dto';
 import { UpdateEmployeesDto } from './dto/update-employees.dto';
 import * as bcrypt from 'bcrypt';
 import { ClientService } from '../client/client.service';
+import { Sector } from 'src/sectors/sector.entity';
 
 @Injectable()
 export class EmployeesService {
@@ -15,32 +16,46 @@ export class EmployeesService {
 
     @Inject(forwardRef(() => ClientService))
     private readonly clientService: ClientService,
+
+    @InjectRepository(Sector)
+    private sectorRepo: Repository<Sector>,
   ) {}
 
   async create(data: CreateEmployeesDto) {
-    const existingEmployee = await this.funcionarioRepo.findOne({
-      where: { email: data.email },
-    });
+  const existingEmployee = await this.funcionarioRepo.findOne({
+    where: { email: data.email },
+  });
 
-    if (existingEmployee) {
-      throw new ConflictException('Email já está registrado como Funcionário.');
-    }
-
-    const existingClient = await this.clientService.findByEmail(data.email);
-
-    if (existingClient) {
-      throw new ConflictException('Email já está registrado como Cliente.');
-    }
-
-    const hash = await bcrypt.hash(data.password, 10);
-
-    const employee = this.funcionarioRepo.create({
-      ...data,
-      password: hash,
-    });
-
-    return this.funcionarioRepo.save(employee);
+  if (existingEmployee) {
+    throw new ConflictException('Email já está registrado como Funcionário.');
   }
+
+  const existingClient = await this.clientService.findByEmail(data.email);
+
+  if (existingClient) {
+    throw new ConflictException('Email já está registrado como Cliente.');
+  }
+
+  const sector = await this.sectorRepo.findOne({
+    where: { id: data.sectorId },
+  });
+
+  if (!sector) {
+    throw new NotFoundException('Setor não encontrado.');
+  }
+
+  const hash = await bcrypt.hash(data.password, 10);
+
+  const employee = this.funcionarioRepo.create({
+    nome: data.nome,
+    email: data.email,
+    password: hash,
+    sector: sector,     // 👈 ESSENCIAL
+  });
+
+  return this.funcionarioRepo.save(employee);
+}
+
 
   findAll() {
     return this.funcionarioRepo.find();
