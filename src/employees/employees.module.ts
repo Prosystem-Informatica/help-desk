cf6ff9e0f3f57824c17892f96281709bcc5d3636
Giovanni Zaghi <vanni.freitas@hotmail.com>
2025-12-06 15:44:26 -0300

    fix: import

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -4,11 +4,14 @@ import { Employees } from './employees.entity';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 import { ClientModule } from '../client/client.module';
+import { SectorsModule } from 'src/sectors/sector.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Employees]),
-    forwardRef(() => ClientModule), ],
+    forwardRef(() => ClientModule), 
+    forwardRef(() => SectorsModule), 
+  ],
   controllers: [EmployeesController],
   providers: [EmployeesService],
   exports: [EmployeesService]
