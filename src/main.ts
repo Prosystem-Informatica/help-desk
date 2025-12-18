1cf76d427bef495071c2f423074533276d8b72bc
Giovanni Zaghi <vanni.freitas@hotmail.com>
2025-12-18 11:57:56 -0300

    fix: try change limit

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ async function bootstrap() {
     }),
   );
 
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+
   await app.listen(env.appPort);
 }
 bootstrap();
