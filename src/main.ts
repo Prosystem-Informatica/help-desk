67c3ecf7e180b2a4db9b8f6df46311eaaa17f88a
Giovanni Zaghi <vanni.freitas@hotmail.com>
2026-01-05 23:21:23 -0300

    revert commit

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,6 @@ async function bootstrap() {
     }),
   );
 
-  app.use(bodyParser.json({ limit: '5mb' }));
-  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
-
   await app.listen(env.appPort);
 }
 bootstrap();
